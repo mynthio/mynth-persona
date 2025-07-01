452dfabb87d51bd77cdadcc210651d6ba3c7dd9b
50BytesOfJohn <93471075+50BytesOfJohn@users.noreply.github.com>
2025-07-01 20:56:54 +0200

    refactor: Update logging configuration to use @logtail/pino (#3)
    - Replaced 'pino-pretty' with '@logtail/pino' in serverExternalPackages for enhanced logging.
    - Modified logger setup to conditionally use JSON format on Vercel production environment, ensuring better logging practices.

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -3,21 +3,21 @@ import pino, { Logger } from "pino";
 // Prevent tree-shaking
 import "@logtail/pino";
 
-const transport = pino.transport({
-  target: "@logtail/pino",
-  options: {
-    sourceToken: process.env.BETTER_STACK_LOGS_SOURCE_TOKEN!,
-    options: {
-      endpoint: `https://${process.env.BETTER_STACK_LOGS_INGESTING_HOST}`,
-    },
-  },
-  level: "debug",
-});
-
 export const logger: Logger =
-  process.env["NODE_ENV"] === "production"
-    ? // JSON in production
-      pino(transport)
+  process.env.VERCEL_ENV === "production"
+    ? // JSON in production, only on Vercel
+      pino(
+        pino.transport({
+          target: "@logtail/pino",
+          options: {
+            sourceToken: process.env.BETTER_STACK_LOGS_SOURCE_TOKEN!,
+            options: {
+              endpoint: `https://${process.env.BETTER_STACK_LOGS_INGESTING_HOST}`,
+            },
+          },
+          level: "debug",
+        })
+      )
     : // Pretty print in development
       pino({
         level: "debug",

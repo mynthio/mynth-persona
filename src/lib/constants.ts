5ef9a39ee10e13ecf00245fb42c9b371d84fb81d
Tomasz Szczuciński <tomasz.szczucinski@mynth.io>
2025-10-05 17:40:37 +0200

    chore: update discord invite url to new server

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,7 +5,7 @@
  */
 export const DAILY_FREE_TOKENS = 10;
 
-export const DISCORD_INVITE_URL = "https://discord.gg/ktHXuPVaqB";
+export const DISCORD_INVITE_URL = "https://discord.gg/kAdUe7zj2b";
 
 export const GITHUB_REPO_URL = "https://github.com/mynthio/mynth-persona";
 

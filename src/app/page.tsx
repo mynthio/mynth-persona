772172b2d0323a32f5c8ede3c0b92202387d6b73
Tomasz Szczuciński <tomasz.szczucinski@mynth.io>
2025-11-01 23:32:27 +0100

    Remove HalloweenSection component from HomePage for a cleaner layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,10 @@ import FloatingOrbs from "@/components/backgrounds/floating-orbs";
 import Home from "./_components/home";
 
 import { GenerationContextProvider } from "@/contexts/generation-context";
-import { HalloweenSection } from "./_components/halloween-section";
 
 export default async function HomePage() {
   return (
     <>
-      <HalloweenSection />
       <GenerationContextProvider>
         <Home />
       </GenerationContextProvider>
